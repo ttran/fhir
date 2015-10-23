c6d47fd17ec033b6197fbeadc3b643f78cdfa248
Thuc Tran <ttran92@gatech.edu>
2015-10-23 14:39:00 -0400

    returns patients

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-exports = function(){
-	console.log('inside fhir node');
+var fhir = require('./controllers/fhir-api');
+var Q = require('q');
+
+module.exports = function(key, start, end){
+	var calls = [];
+	
+	for (var i=start;i<end;i+=50){
+		if (i+50>end){
+			var count = end-i;
+			calls.push(fhir(i, count));
+		} else {
+			calls.push(fhir(i, 50));
+		}
+	}
+	
+	var api = Q.all(calls);
+	
+	api.then(function(data){
+		var patients = [];
+		for (var i=0;i<data.length;i++){
+			for (var e=0;e<data[i].entry.length;e++){
+				patients.push(data[i].entry[e]);
+			}
+		}
+		return patients;
+	});
 };
