0d8f8e5abcee0245555b16bdb5969136ec8c6fc6
Thuc Tran <ttran92@gatech.edu>
2015-10-26 15:34:21 -0400

    flexible and works now

--- index.js
+++ index.js
@@ -1,46 +1,59 @@
 var fhir = require('./controllers/fhir-api');
+var rp = require('request-promise');
 var Q = require('q');
 
 module.exports = function(options, callback){
-	var pages = options.pages;
-	var start = options.start;
-	var end = options.end;
-	var format = options.format;
-	var url ='';
-	
-	var calls = [];
-	var deferred = Q.defer();
-	for (var offset=start;offset<end;offset+=50){
-		var count = 50;
-		if (offset+50>end){
-			count = end-offset;
-		}
-		url = 'http://polaris.i3l.gatech.edu:8080/gt-fhir-webapp-ro/base?_getpages='+pages+
-		'&_getpagesoffset='+offset+
-		'&_count='+count+
-		'&_format='+format+
-		'&_pretty=true';
-		calls.push(fhir(url));
-	}
-	
-	var api = Q.all(calls);
-	
-	api.then(function(data){
-		var patients = [];
-		for (var i=0;i<data.length;i++){
-			for (var e=0;e<data[i].entry.length;e++){
-				patients.push(data[i].entry[e]);
+	var defer = Q.defer();
+	rp('http://polaris.i3l.gatech.edu:8080/gt-fhir-webapp-ro/base/Patient?_format=json&_pretty=true&_count=0').then(function(res){
+		var data = JSON.parse(res);
+		var key = data.link[1].url;
+		key = key.split('_getpages=')[1];
+		key = key.split('&_getpagesoffset=')[0];
+		var start = 0;
+		var end = data.total;
+		var format = 'json';
+			if (options!=null){
+				start = options.start;
+				end = options.end;
+				format = options.format;
 			}
+		
+		var url;
+		var calls = [];
+		var deferred = Q.defer();
+		for (var offset=start;offset<end;offset+=50){
+			var count = 50;
+			if (offset+50>end){
+				count = end-offset;
+			}
+			url = 'http://polaris.i3l.gatech.edu:8080/gt-fhir-webapp-ro/base?_getpages='+key+
+			'&_getpagesoffset='+offset+
+			'&_count='+count+
+			'&_format='+format+
+			'&_pretty=true';
+			calls.push(fhir(url));
 		}
-		// runs callback if present
+		
+		var api = Q.all(calls);
+		
+		api.then(function(info){
+			var patients = [];
+			for (var i=0;i<info.length;i++){
+				for (var e=0;e<info[i].entry.length;e++){
+					patients.push(info[i].entry[e]);
+				}
+			}
+			// runs callback if present
+			if (!callback){
+				deferred.resolve(patients);
+			} else{
+				callback(patients);
+			}
+		});
+		// returns promise
 		if (!callback){
-			deferred.resolve(patients);
-		} else{
-			callback(patients);
+			defer.resolve(deferred.promise);
 		}
 	});
-	// returns promise
-	if (!callback){
-		return deferred.promise;
-	}
+	return defer.promise;
 };
